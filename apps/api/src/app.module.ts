969032a656f8bba47c85e2843a50cf516b3a4c2a
Diego Petitto <dmpetitto2004@gmail.com>
2026-02-09 00:35:08 -0300

    Initialize backend api project with nestjs cli

---
+++ apps/api/src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ProductsModule } from './products/products.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    ProductsModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}

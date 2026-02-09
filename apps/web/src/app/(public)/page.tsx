28057f28a226aae07b4d2a54f77119eb87676981
Diego Petitto <dmpetitto2004@gmail.com>
2026-02-09 01:12:01 -0300

    Add a navigation link to the calculator

--- apps/web/src/app/(public)/page.tsx
+++ apps/web/src/app/(public)/page.tsx
@@ -1,3 +1,12 @@
+import Link from "next/link";
+
 export default function Page() {
-  return <h1>Hello, Neon Nova!</h1>;
+  return (
+    <>
+      <h1>Hello, Neon Nova!</h1>
+      <Link href="/calculadora">
+        <button className="text-violet-600 bg-violet-100 cursor-pointer border p-2 rounded-2xl">Go to Calculator</button>
+      </Link>
+    </>
+  );
 }

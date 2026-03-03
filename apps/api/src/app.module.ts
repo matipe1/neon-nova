501651f1b5466d132c9d9fb1ee29e429da7ab685
Diego Petitto <dmpetitto2004@gmail.com>
2026-03-03 16:00:39 -0300

    Cleaning up backend project

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    ProductsModule,
   ],
   controllers: [],
   providers: [],

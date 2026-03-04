ba2d5c2ca41d396213941f9a00857ecd8d3fab72
Diego Petitto <dmpetitto2004@gmail.com>
2026-03-03 23:54:23 -0300

    Simplify AppModule imports

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,14 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
+import { TagsModule } from './modules/tags/tags.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-    PrismaModule,
-  ],
+  imports: [PrismaModule, TagsModule],
   controllers: [],
   providers: [],
 })

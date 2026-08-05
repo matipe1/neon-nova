f5da5eaebf8721ab00f4f2664be2dca0bc57d1fd
Diego Petitto <dmpetitto2004@gmail.com>
2026-08-05 16:38:36 -0300

    feat: import tanstack router library

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    TanStackRouterVite({
+      autoCodeSplitting: true, // Enable automatic code splitting
+    }),
     react(),
     babel({ presets: [reactCompilerPreset()] })
   ],

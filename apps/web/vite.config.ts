c34de47577b390ae8e23d06df5981b51e38950a3
Diego Petitto <dmpetitto2004@gmail.com>
2026-08-05 16:51:00 -0300

    feat: import Tailwind CSS framework

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -2,13 +2,15 @@ import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     TanStackRouterVite({
-      autoCodeSplitting: true, // Enable automatic code splitting
+      autoCodeSplitting: true,
     }),
+    tailwindcss(),
     react(),
     babel({ presets: [reactCompilerPreset()] })
   ],

6c00756dc48f6ca2c9db70b62ab4e9a43bf284d8
Diego Petitto <dmpetitto2004@gmail.com>
2026-08-08 01:31:16 -0300

    feat: import tanstack query library

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,12 +1,17 @@
 import { RouterProvider, createRouter } from '@tanstack/react-router';
 import { routeTree } from './routeTree.gen';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 const router = createRouter({ routeTree })
 
 const App = () => {
   return (
     <>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </>
   )
 }
--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -1,6 +1,7 @@
 import AppLayout from '../components/layout/AppLayout'
 import { createRootRoute, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/router-devtools'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 export const Route = createRootRoute({
     component: () => {
@@ -9,6 +10,7 @@ export const Route = createRootRoute({
                 <AppLayout>
                     <Outlet />
                     <TanStackRouterDevtools />
+                    <ReactQueryDevtools />
                 </AppLayout>
             </>
         )
